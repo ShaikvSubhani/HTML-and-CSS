8d70e1c062b537df855d38c070aa78628839ae5a
ShaikvSubhani <shaik4321subhani@gmail.com>
2023-03-29 08:22:43 +0530

    update

---
+++ Js2/index.js
@@ -0,0 +1,47 @@
+// typecasting in javascript
+
+let a =20;
+let b=String(a);
+console.log(b,typeof(b));
+
+
+//conditional statements
+
+// if(true)
+// {
+//     console.log("inside if")
+// }
+// else if(true)
+// {
+//     console.log("inside if" )
+// }
+// else{
+//     console.log("finally")
+// }
+
+
+// let x="male";
+
+// switch(x)
+// {
+//     case "male":{
+//         console.log("he is male")
+//         break
+//     }
+
+//     case "female":{
+//         console.log("he is female")
+//         break
+//     }
+
+//     case "other":{
+//         console.log("none of the above ")
+//         break
+//     }
+// }
+
+for(let i=0;i<10;i++)
+{
+    console.log(i);
+}
+
